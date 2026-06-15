4c84a0dc77ec3b784b29742c553021f44b4c6e7e
Yash Rathore <yshrthr0@gmail.com>
2026-06-15 12:57:17 +0530

    categories UI

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import {LuShoppingBag} from 'react-icons/lu'
 
 function Nav() {
   return (
-    <div className='w-full h-[100px] bg-red-50 flex justify-between items-center px-8'>
+    <div className='w-full h-[100px] flex justify-between items-center px-8'>
         <div className='w-[60px] h-[60px] bg-white flex justify-center items-center rounded-md shadow-xl'>
             <MdFastfood className='w-[30px] h-[30px] text-green-500'/>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Categories from '../Categories'
 
 function Home() {
   return (
-    <div className='w-full min-h-screen bg-slate-400'>
+    <div className='w-[100%] min-h-screen bg-red-50'>
         <Nav />
+        <div className='w-full flex flex-wrap justify-center items-center gap-4 w-[100%]'>
+          {Categories.map((item) =>   {
+            return <div className='w-[120px] h-[130px] bg-white flex flex-col item-start gap-2 p-5 justify-start text-[17px] text-grey-600 font-semibold rounded-md shadow-xl hover:bg-green-200 cursor-pointer transition-all'>
+              {item.icon}
+              {item.name}
+            </div>
+          })}
+        </div>
     </div>
   )
 }
